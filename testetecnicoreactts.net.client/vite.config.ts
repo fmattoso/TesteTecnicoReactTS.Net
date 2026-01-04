e71f42055e43ceba53e0b854fbba75e51204f902
fmattoso <fabiano.mattoso@gmail.com>
2026-01-04 17:03:05 -0300

    implantacao

--- testetecnicoreactts.net.client/vite.config.ts
+++ testetecnicoreactts.net.client/vite.config.ts
@@ -52,7 +52,7 @@ export default defineConfig({
                 secure: false
             }
         },
-        port: parseInt(env.DEV_SERVER_PORT || '52462'),
+        port: parseInt(env.DEV_SERVER_PORT || '52463'),
         https: {
             key: fs.readFileSync(keyFilePath),
             cert: fs.readFileSync(certFilePath),
